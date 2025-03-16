ed69a42e696be6a9e5aaf7eaefc2adeca893b1a8
kallebsous <kallebaraujo8@gmail.com>
2025-03-16 16:03:38 -0300

    add icon e title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Formulário #INCLUDE <MUDANÇA>',
+  description: 'Coletando sugestões em busca da melhoria',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.ico', // Caminho padrão
+  }
 }
 
 export default function RootLayout({
